1711f1e94cd45b7ef8a910de19442aaa117a5837
vicentecarlos <soniamariadias2012@hotmail.com>
2022-12-22 20:33:36 -0300

    feat: implements mobile first

---
+++ assets/js/script copy.js
@@ -0,0 +1,74 @@
+const countdown = document.querySelector(".countdown");
+// let date = String(new Date()).split(" ");
+// let exactHour = date[4].split(":")
+// let span, text, dateForSite = [date[2], exactHour[0], exactHour[1], exactHour[2]];
+// let calcDateForEvent = 
+// console.log(dateForSite)
+
+
+
+
+
+
+// for (let i=1; i<=4; i++) {
+//     span = document.createElement("span");
+//     span.classList.add(`span-${i}`)
+//     if (span.classList.contains("span-1")) {
+//         text = document.createTextNode(`${dateForSite[0]}D`)
+//     } else if (span.classList.contains("span-2")) {
+//         text = document.createTextNode(`${dateForSite[1]}H`)
+//     } else if (span.classList.contains("span-3")) {
+//         text = document.createTextNode(`${dateForSite[2]}M`)
+//     } else {
+//         text = document.createTextNode(`${dateForSite[3]}S`)
+//     }
+//     span.appendChild(text)
+//     countdown.appendChild(span)
+// }
+
+// ---------------------------------------
+
+// const countdown = document.querySelector(".countdown");
+
+// const everySecond = initialSecond => {
+//     let second = initialSecond
+//     setInterval(() => {
+//         if (second === 0) {
+//             second = initialSecond
+//         }
+//         countdown.innerHTML = second
+//         return --second
+//     }, 1000)
+// }
+
+// for (let i=1; i<=4; i++) {
+//     span = document.createElement("span");
+//     span.classList.add(`span-${i}`)
+//     if (span.classList.contains("span-1")) {
+//         text = document.createTextNode(`${dateForSite[0]}D`)
+//     } else if (span.classList.contains("span-2")) {
+//         text = document.createTextNode(`${dateForSite[1]}H`)
+//     } else if (span.classList.contains("span-3")) {
+//         text = document.createTextNode(`${dateForSite[2]}M`)
+//     } else {
+//         text = document.createTextNode(`${dateForSite[3]}S`)
+//     }
+//     span.appendChild(text)
+//     countdown.appendChild(span)
+// }
+
+
+
+// everySecond(10)
+
+
+
+
+
+
+
+
+
+
+
+
